aeb8a89e1aa575d37dd017f0fe30aafa30240d42
joshuab1000 <joshuab1000@yahoo.com>
2021-12-04 23:28:32 -0500

    Update main.py

--- day2/main.py
+++ day2/main.py
@@ -1,1 +1,19 @@
+horizontalPos = 0
+depth = 0
 
+#Part 1
+with open('input.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        instruction = line.split()
+        num = int(instruction[1])
+        direction = instruction[0]
+
+        if direction == 'forward':
+            horizontalPos += num
+        elif direction == 'up':
+            depth -= num
+        elif direction == 'down':
+            depth += num
+
+    print("Depth * Horizontal Position =", (depth * horizontalPos))
